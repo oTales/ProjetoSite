cb4badbf36923c7b652bf5d20d8e48f281313ce0
Tales-707 <talesirineu@gmail.com>
2023-05-30 22:22:34 -0300

    subindo components e alteracoes de maps na parte de contato

---
+++ WebComponents/contato.php
@@ -0,0 +1,83 @@
+<section id="contact" class="contact">
+  <div class="container" data-aos="fade-up">
+
+    <div class="section-header">
+      <h2>Contact</h2>
+      <p>Need Help? <span>Contact Us</span></p>
+    </div>
+
+    <div class="mb-3">
+<iframe width="100%" height="400" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3775.7816900754606!2d-41.9807197!3d-18.8523723!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xb1a660f1ff49a3%3A0x386f8a205bb07117!2sR.%20Cedro%2C%20167%20-%20Turmalina%2C%20Gov.%20Valadares%20-%20MG%2C%2035065-011!5e0!3m2!1spt-BR!2sbr!4v1685495778467!5m2!1spt-BR!2sbr" > </iframe>       
+    </div><!-- End Google Maps -->
+
+    <div class="row gy-4">
+
+      <div class="col-md-6">
+        <div class="info-item  d-flex align-items-center">
+          <i class="icon bi bi-map flex-shrink-0"></i>
+          <div>
+            <h3>Our Address</h3>
+            <p>A108 Adam Street, New York, NY 535022</p>
+          </div>
+        </div>
+      </div><!-- End Info Item -->
+
+      <div class="col-md-6">
+        <div class="info-item d-flex align-items-center">
+          <i class="icon bi bi-envelope flex-shrink-0"></i>
+          <div>
+            <h3>Email Us</h3>
+            <p>contact@example.com</p>
+          </div>
+        </div>
+      </div><!-- End Info Item -->
+
+      <div class="col-md-6">
+        <div class="info-item  d-flex align-items-center">
+          <i class="icon bi bi-telephone flex-shrink-0"></i>
+          <div>
+            <h3>Call Us</h3>
+            <p>+1 5589 55488 55</p>
+          </div>
+        </div>
+      </div><!-- End Info Item -->
+
+      <div class="col-md-6">
+        <div class="info-item  d-flex align-items-center">
+          <i class="icon bi bi-share flex-shrink-0"></i>
+          <div>
+            <h3>Opening Hours</h3>
+            <div><strong>Mon-Sat:</strong> 11AM - 23PM;
+              <strong>Sunday:</strong> Closed
+            </div>
+          </div>
+        </div>
+      </div><!-- End Info Item -->
+
+    </div>
+
+    <form action="forms/contact.php" method="post" role="form" class="php-email-form p-3 p-md-4">
+      <div class="row">
+        <div class="col-xl-6 form-group">
+          <input type="text" name="name" class="form-control" id="name" placeholder="Your Name" required>
+        </div>
+        <div class="col-xl-6 form-group">
+          <input type="email" class="form-control" name="email" id="email" placeholder="Your Email" required>
+        </div>
+      </div>
+      <div class="form-group">
+        <input type="text" class="form-control" name="subject" id="subject" placeholder="Subject" required>
+      </div>
+      <div class="form-group">
+        <textarea class="form-control" name="message" rows="5" placeholder="Message" required></textarea>
+      </div>
+      <div class="my-3">
+        <div class="loading">Loading</div>
+        <div class="error-message"></div>
+        <div class="sent-message">Your message has been sent. Thank you!</div>
+      </div>
+      <div class="text-center"><button type="submit">Send Message</button></div>
+    </form><!--End Contact Form -->
+
+  </div>
+</section>
---
+++ index.php
@@ -0,0 +1,114 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+  <title>Pizza Pizza Pizza</title>
+  <meta content="Pizza boa e barata" name="description">
+  <meta content="" name="keywords">
+
+  <!-- Favicons -->
+  <link href="assets/img/favicon.png" rel="icon">
+  <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+
+  <!-- Google Fonts -->
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,600;1,700&family=Amatic+SC:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&family=Inter:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&display=swap" rel="stylesheet">
+
+  <!-- Vendor CSS Files -->
+  <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+  <link href="assets/vendor/aos/aos.css" rel="stylesheet">
+  <link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+  <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+
+  <!-- Template Main CSS File -->
+  <link href="assets/css/main.css" rel="stylesheet">
+
+  <!-- =======================================================
+  * Template Name: Yummy
+  * Updated: Mar 10 2023 with Bootstrap v5.2.3
+  * Template URL: https://bootstrapmade.com/yummy-bootstrap-restaurant-website-template/
+  * Author: BootstrapMade.com
+  * License: https://bootstrapmade.com/license/
+  ======================================================== -->
+</head>
+
+<body>
+  <!-- MENU/NAVLIST -->
+  <?php include_once './WebComponents/menu.php'?>
+  
+<!-- BANNER -->
+  <?php include_once './WebComponents/banner.php'?>
+
+  <main id="main">
+
+
+    <!-- SOBRE NOS -->
+    <?php include_once './WebComponents/sobrenos.php'?>
+    
+
+
+
+    <!-- CARDS -->
+    <?php include_once './WebComponents/cards.php'?>
+
+  <!-- STATUS COM banner -->
+  <?php include_once './WebComponents/status.php'?>
+
+
+    <!-- CARDAPIO -->
+    <?php include_once './WebComponents/cardapio.php'?>
+
+    <!-- AVALIACOES -->
+      <?php include_once './WebComponents/avaliacoes.php' ?>
+
+ 
+      <!-- EVENTOS -->
+    <?php include_once './WebComponents/eventos.php'?>
+
+
+
+    <!-- CHEFES -->
+    <?php include_once './WebComponents/chefes.php'?>
+
+
+ 
+    <!-- FACA SUA RESERVA -->
+    <?php include_once './WebComponents/reserva.php'?>
+
+
+   
+    <!-- GALERIA -->
+    <?php include_once './WebComponents/galeria.php'?>
+   
+
+    <!-- CONTATO -->
+    <?php include_once './WebComponents/contato.php'?>
+
+
+  </main>
+<!-- FOOTER -->
+  <?php include_once './WebComponents/footer.php'?>
+
+
+  <a href="#" class="scroll-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+
+  <div id="preloader"></div>
+  <!-- Vendor -->
+  <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="assets/vendor/aos/aos.js"></script>
+  <script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
+  <script src="assets/vendor/purecounter/purecounter_vanilla.js"></script>
+  <script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
+  <script src="assets/vendor/php-email-form/validate.js"></script>
+
+  <!-- Template Main JS File -->
+  <script src="assets/js/main.js"></script>
+
+</body>
+
+</html>
